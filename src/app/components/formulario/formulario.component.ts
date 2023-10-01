70e2b3e6b84f08ce8334e7905077a2e242b59203
SegundoPaladines <segundopaladines@gmail.com>
2023-10-01 11:51:45 -0500

    Local Reference

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -10,14 +10,14 @@ export class FormularioComponent {
 
   /* declaracion del output con el event emiter que permite usar la funcion emit() para enviar la info*/
   /* personaCreada se convierte en un evento escuchable por otros componentes*/
-  
+
   @Output() personaCreada = new EventEmitter<Persona>();
 
-  inputNombre = '';
-  inputApellido = '';
+  /* como la referencia se hace al input entero se debe recibir como HTMLInputElement*/
 
-  agregarPersona(){
+  agregarPersona(nombre: HTMLInputElement, apellido:HTMLInputElement){
+    /* para obtener el valor del input se usa el .value */
     /* enviar la persona al componente que contenga este componente*/
-    this.personaCreada.emit(new Persona(this.inputNombre, this.inputApellido));
+    this.personaCreada.emit(new Persona(nombre.value, apellido.value));
   }
 }
